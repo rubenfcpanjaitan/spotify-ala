a332c1aefa1c6b6149d1701e51b5e915fc2070e8
trisnaadetia <trisnaadetia@gmail.com>
2022-04-22 16:17:09 +0700

    [ADD] create playlist

--- src/components/CreatePlaylist.js
+++ src/components/CreatePlaylist.js
@@ -1,24 +1,88 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
+import axios from "axios";
 import { FaSearch } from "react-icons/fa";
 import {BsTextarea} from "react-icons/bs";
+import { useStateProvider } from "../helper/StateProvider";
+import { reducerCases } from "../helper/Constants";
 
 export default function CreatePlaylist({ createPlaylistBackground }) {
+  const [{ userInfo, token }, dispatch] = useStateProvider();
+  const [newPlaylist, setNewPlaylist] = useState({
+    "name": "",
+    "description": "",
+    "public": false
+  })
+  
+  const createNewPlaylist = async () => {
+    await axios({
+      method: "POST",
+      url: `https://api.spotify.com/v1/users/${userInfo.userId}/playlists`,
+      data: newPlaylist,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    })
+    await getPlaylistData()
+    setNewPlaylist({
+      "name": "",
+      "description": "",
+      "public": false
+    })
+  };
+
+  const getPlaylistData = async () => {
+    const response = await axios.get(
+      "https://api.spotify.com/v1/me/playlists",
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const { items } = response.data;
+    const playlists = items.map(({ name, id }) => {
+      return { name, id };
+    });
+    dispatch({ type: reducerCases.SET_PLAYLISTS, playlists });
+  };
+
+  function handleChange(evt) {
+    const value = evt.target.value;
+    setNewPlaylist({
+      ...newPlaylist,
+      [evt.target.name]: value
+    });
+  }
+
     return (
         <Container>
             <div className="row">
-                <h1>Tambahkan Playlist</h1>
+                <h1 className="title">Tambahkan Playlist</h1>
             </div>
             <div className="row">
                 <div className="column">
                     <div className={"input_playlist"}>
-                        <input name="playlist" placeholder={"nama playlist"}></input>
+                        <input 
+                          name="name" 
+                          placeholder={"nama playlist"}
+                          onChange={handleChange}
+                        />
                     </div>
                 </div>
                 <div className="column">
                     <div className={"input_playlist"}>
-                        <input name={"deskripsi"} placeholder={"Deskripsi"}></input>
+                        <input 
+                          name="description"
+                          placeholder={"Deskripsi"}
+                          onChange={handleChange}
+                        />
                     </div>
+                </div>
+                <div className="column">
+                    <button onClick={createNewPlaylist}>ADD</button>
                 </div>
             </div>
         </Container>
@@ -65,4 +129,7 @@ const Container = styled.div`
     padding: 1px;
     height: 100%; 
   }
+  .title {
+    color: white;
+  } 
 `;
